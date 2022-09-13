f478fed40f5c750a242e3ccc8094b286491c4c2d
effi1994 <effi130894@gmail.com>
2022-09-13 20:56:22 +0300

    add compan heda table

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,16 +5,19 @@ import ListGroup from "../components/common/listGroup";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/pagination";
 import MoviesTable from "./moviesTable";
+import _ from "lodash";
+
 class Movies extends Component {
   state = {
     movies: [],
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    sortColumn: { path: "title", order: "asc" },
   };
 
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
     this.setState({ movies: getMovies(), genres: genres });
   }
 
@@ -38,24 +41,23 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    this.setState({ selecetdGenre: genre ,currentPage:1 });
+    this.setState({ selecetdGenre: genre, currentPage: 1 });
   };
 
-  handleSort=(path)=>{
-    console.log(path)
-  }
-  
+  handleSort = (sortColumn) => {
+    this.setState({sortColumn});
+  };
+
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, selecetdGenre } = this.state;
+    const { pageSize, currentPage, selecetdGenre,sortColumn ,movies:allMovies} = this.state;
     if (count === 0) return <p>There are no movies in the database</p>;
     const filtered =
       selecetdGenre && selecetdGenre._id
-        ? this.state.movies.filter((m) => m.genre._id === selecetdGenre._id)
-        : this.state.movies;
-
-    const movies = paginate(filtered, currentPage, pageSize);
-    console.log(movies);
+        ? allMovies.filter((m) => m.genre._id === selecetdGenre._id)
+        : allMovies;
+    const sorted=  _.orderBy(filtered,[sortColumn.path],[sortColumn.order])
+    const movies = paginate(sorted, currentPage, pageSize);
     return (
       <div className="row">
         <div className="col-3">
@@ -68,10 +70,11 @@ class Movies extends Component {
         <div className="col">
           <p>Showing {filtered.length} moves in database</p>
           <MoviesTable
-           movies={movies}
-           onDelete={this.handleDelete}
-           onLike={this.handleLike}
-           onSort={this.handleSort}
+            movies={movies}
+            sortColumn={sortColumn}
+            onDelete={this.handleDelete}
+            onLike={this.handleLike}
+            onSort={this.handleSort}
           />
           <Pagination
             itemsCount={filtered.length}
